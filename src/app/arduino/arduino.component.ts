fbac9e99d1197f0024049c1f403e8d66798928b9
Benjamin <40469901+BenjaminC137@users.noreply.github.com>
2018-10-12 20:46:34 -0500

    puttin up some arduinos

--- src/app/arduino/arduino.component.ts
+++ src/app/arduino/arduino.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ArduinoProjectModel } from '../arduino-project-model';
 @Component({
   selector: 'app-arduino',
   templateUrl: './arduino.component.html',
   styleUrls: ['./arduino.component.css']
 })
-export class ArduinoComponent implements OnInit {
 
-	arduino : ArduinoProjectModel = {
-		id: 1,
-		title: 'Neechi Feeder',
-		description: 'I designed a dog feeder and built the entire machine using a stepper motor, a servo motor, an LCD screen, an RTC module, custom home-made metal frame and agitator, arduino nano and everything else I used...',
-		dateCreated: '2018/01/01',
-		dateCompleted: '2018/01/02',
-		imagePath: 'assets/rick-prison.png'
-	}
+export class ArduinoComponent implements OnInit {
+	@Input() arduino: ArduinoProjectModel;
+
+//	arduino : ArduinoProjectModel = {
+//		id: 1,
+//		title: 'Neechi Feeder',
+//		description: 'I designed a dog feeder and built the entire machine using a stepper motor, a servo motor, an LCD screen, an RTC module, custom home-made metal frame and agitator, arduino nano and everything else I used...',
+//		dateCreated: '2018/03/21',
+//		dateCompleted: '2018/04/18',
+//		imagePath: "assets/images/ArduinoNeechiFeeder/IMG_7441.png"
+//	}
+
+
+
 
   constructor() { }
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ArduinoProjectModel } from '../arduino-project-model';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+	arduinoProjects : ArduinoProjectModel[] = [
+		{
+		id: 1,
+		title: 'Neechi Feeder',
+		description: 'I designed a dog feeder and built the entire machine using a stepper motor, a servo motor, an LCD screen, an RTC module, custom home-made metal frame and agitator, arduino nano and everything else I used...',
+		dateCreated: '2018/03/21',
+		dateCompleted: '2018/04/18',
+		imagePath: "assets/ArduinoNeechiFeeder/Neechi-full-IMG_8278.png"
+		},
+		{
+		id: 2,
+		title: 'Buddy (Smart Car)',
+		description: 'A car that roams around the house for companionship so people don\'t feel lonely.',
+		dateCreated: '2016/01/01',
+		dateCompleted: '2016/01/02',
+		imagePath: "assets/Buddy/buddy-full-IMG_0423.png"
+		},
+		{
+		id: 3,
+		title: 'Crunch Counter',
+		description: 'Counts crunches while I exercise so I can watch TV and not have to count. I lose track when I count anyway because I start thinking about other things.',
+		dateCreated: '2018/10/05',
+		dateCompleted: '2018/10/08',
+		imagePath: "assets/images/ArduinoNeechiFeeder/IMG_7441.png"
+		},
+		{
+		id: 4,
+		title: 'Fridge Alarm',
+		description: 'Box on fridge buzzes and flashes when open too long or it doesn\'t fully close.',
+		dateCreated: '2017/01/01',
+		dateCompleted: '2017/01/01',
+		imagePath: "assets/ArduinoTemperatureAlarm/temperature-alarm-IMG_7652.png"
+		},
+		{
+		id: 5,
+		title: 'Home Box',
+		description: 'Automatically turns on lights when you walk by. Uses the unused buttons on the TV remote to turn on or off. Has manual buttons.',
+		dateCreated: '2017/01/01',
+		dateCompleted: '2017/01/01',
+		imagePath: "assets/ArduinoHomeBox/Homebox-complete-IMG_8136.png"
+		},
+		{
+		id: 6,
+		title: 'Smart-tank',
+		description: 'Analyzes surroundings and chooses where to go.',
+		dateCreated: '2017/01/01',
+		dateCompleted: '2017/01/01',
+		imagePath: "assets/images/ArduinoNeechiFeeder/IMG_7441.png"
+		},
+		{
+		id: 7,
+		title: 'Security System',
+		description: 'Security System.',
+		dateCreated: '2017/01/01',
+		dateCompleted: '2017/01/01',
+		imagePath: "assets/images/ArduinoNeechiFeeder/IMG_7441.png"
+		}
+	];
+
+
 
   constructor() { }
 
