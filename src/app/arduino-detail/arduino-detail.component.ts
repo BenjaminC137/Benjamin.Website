4bb142a8e7f4e2cca653457addbcd75d1c62e434
Benjamin <40469901+BenjaminC137@users.noreply.github.com>
2018-10-29 16:31:16 -0500

    implemented zoom for images

--- src/app/arduino-detail/arduino-detail.component.ts
+++ src/app/arduino-detail/arduino-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit, NgModule } from '@angular/core';
 import { ArduinoProjectModel } from '../arduino-project-model';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
@@ -6,19 +6,34 @@ import { ArduinoService } from '../arduino.service';
 import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 import { ArduinoProjects } from '../arduino-projects';
 
+@NgModule({
+//  declarations: [SomeComponent, SomeDirective, SomePipe],
+//  providers: [arduinoService],
+//  exports: [CommonModule],
+//  imports: [CommonModule]
+
+})
+
 @Component({
   selector: 'app-arduino-detail',
   templateUrl: './arduino-detail.component.html',
   styleUrls: ['./arduino-detail.component.css']
 })
 
-export class ArduinoDetailComponent implements OnInit {
+export class ArduinoDetailComponent implements OnInit{
+// @ViewChild('i') i: ElementRef;
 //	@Input() arduino: ArduinoProjectModel;
+//	@Input() show: boolean;
+
 	arduino: ArduinoProjectModel;
+//	selectedIdx: number;
 	goPlay: string;
 	trustedDashboardUrl : SafeUrl;
-//	currentImage = 0;
-  constructor(
+	zoomState:boolean = false;
+	selectedIdx: number = null;
+	 show: boolean = false;
+
+	constructor(
 	  private route: ActivatedRoute,
 	  private arduinoService: ArduinoService,
 	  private location: Location,
@@ -31,7 +46,7 @@ export class ArduinoDetailComponent implements OnInit {
   ngOnInit(): void {
 	  this.getArduino();
 	  this.goPlay = this.arduino.videoUrl;
-
+//	  this.selectedIdx = 0;
   }
 	getArduino(): void {
 		const id = +this.route.snapshot.paramMap.get('id');
@@ -40,4 +55,28 @@ export class ArduinoDetailComponent implements OnInit {
 	goBack(): void{
 		this.location.back();
 	}
+//	 ngAfterViewInit() {
+//      console.log(this.myId.nativeElement);
+//		this.selectedIdx: int = 0;
+
+	ViewHiRes(index: number){
+		if(this.show == true){
+		this.show = !this.show
+		this.selectedIdx = null;
+		}
+		else if (this.show == false{
+		this.selectedIdx = index;
+		console.log(index);
+		this.show = !this.show
+		}
+//			this.zoomState[i] = !this.zoomState[i];
+//			if(this.zoom == false){
+//				this.i.zoomState = "img-scale";
+//			}
+//			else{
+//				this.i.zoomState = "view-hi-res"
+//			}
+	}
+//    }
+
 }
--- src/fireball.js
+++ src/fireball.js
@@ -1,145 +1,145 @@
-document.onkeypress = function(e){
-
-	var box = document.getElementsByClassName ("box")[0];
-
-	var top = window.getComputedStyle(box.nativeElement).top;
-
-	top = Number(top.replace("px", ""));
-
-	var left = window.getComputedStyle(box.nativeElement).left;
-	left = Number(left.replace("px", ""));
-
-	var posx = window.getComputedStyle(box.nativeElement).backgroundPositionX;
-	posx = Number(posx.replace("px", ""));
-
-	var posy = window.getComputedStyle(box.nativeElement).backgroundPositionY;
-	posy = Number(posy.replace("px", ""));
-
-	if(left < 1){ // if hit left
-		left = 1;
-		left += "px"
-		box.style.left = left;
-	}
-
-	if(left > window.innerWidth - 50){ // if hit right
-		left = window.innerWidth - 55;
-		left += "px"
-		box.style.left = left;
-	}
-
-	if(top < 1){ // if hit top
-		top = 1;
-		top += "px"
-		box.style.top = top;
-	}
-
-	if(top > window.innerHeight - 70){ // if hit bottom
-		top = window.innerHeight - 76;
-		top += "px"
-		box.style.top = top;
-	}
-
-	if(e.key == 'd'){ //move right
-		left += 20;
-		left += "px"
-		box.style.left = left;
-
-		posy = 320;
-		posx += 64;
-		posx += "px";
-		posy += "px";
-		box.style.backgroundPositionX = posx;
-		box.style.backgroundPositionY = posy;
-
-	}
-
-	if(e.key == 's'){ //move down
-		top += 20;
-		top += "px"
-		box.style.top = top;
-
-		posy = 384;
-		posx += 64;
-		posx += "px";
-		posy += "px";
-		box.style.backgroundPositionX = posx;
-		box.style.backgroundPositionY = posy;
-
-
-	}
-
-	if(e.key == 'a'){ //move left
-	left -= 20;
-	left += "px"
-	box.style.left = left;
-
-	posx -= 64;
-	posx += "px";
-	posy = 192;
-	posy += "px";
-	box.style.backgroundPositionX = posx;		box.style.backgroundPositionY = posy;
-
-	}
-
-	if(e.key == 'w'){ //move up
-		top -= 20;
-		top += "px"
-		box.style.top = top;
-
-		posy = 256;
-		posx += 64;
-		posx += "px";
-		posy += "px";
-		box.style.backgroundPositionX = posx;
-		box.style.backgroundPositionY = posy;
-	}
-    console.log(e.key);
-    }
-
-document.onclick = function (coords){
-	var fireball = document.getElementsByClassName("fireball")[0];
-
-
-
-
-	var box = document.getElementsByTagName("box")[0];
-	var top = window.getComputedStyle(box.nativeElement).top;
-	top = Number(top.replace("px", ""));
-	var left = window.getComputedStyle(box.nativeElement).left;
-	left = Number(left.replace("px", ""));
-
-
-
-
-	var xcoord = coords.x - 16;
-	var ycoord = coords.y - 16;
-
-		xcoord += "px";
-		ycoord += "px";
-		fireball.style.left = xcoord;
-		fireball.style.top = ycoord;
-
-if(((coords.x < left + 51) && (coords.x > left + 13)) && ((coords.y < top + 66) && (coords.y > top + 13))){
-	console.log("hit");
-	box.style.backgroundColor = "orange";
-}
-	else{
-		box.style.backgroundColor = null;
-	}
-}
-
-function getFormValues(){
-	var f = document.getElementsByName("f")[0];
-	var l = document.getElementsByName("l")[0];
-
-	console.log(f.value);
-    console.log(l.value);
-}
-
-$("p").mouseover(e => {
-	$("body").css("background-color", "yellow");
-})
-
-$("p").mouseout(e => {
-	$("body").css("background-color", "inherit");
-})
+//document.onkeypress = function(e){
+//
+//	var box = document.getElementsByClassName ("box")[0];
+//
+//	var top = window.getComputedStyle(box.nativeElement).top;
+//
+//	top = Number(top.replace("px", ""));
+//
+//	var left = window.getComputedStyle(box.nativeElement).left;
+//	left = Number(left.replace("px", ""));
+//
+//	var posx = window.getComputedStyle(box.nativeElement).backgroundPositionX;
+//	posx = Number(posx.replace("px", ""));
+//
+//	var posy = window.getComputedStyle(box.nativeElement).backgroundPositionY;
+//	posy = Number(posy.replace("px", ""));
+//
+//	if(left < 1){ // if hit left
+//		left = 1;
+//		left += "px"
+//		box.style.left = left;
+//	}
+//
+//	if(left > window.innerWidth - 50){ // if hit right
+//		left = window.innerWidth - 55;
+//		left += "px"
+//		box.style.left = left;
+//	}
+//
+//	if(top < 1){ // if hit top
+//		top = 1;
+//		top += "px"
+//		box.style.top = top;
+//	}
+//
+//	if(top > window.innerHeight - 70){ // if hit bottom
+//		top = window.innerHeight - 76;
+//		top += "px"
+//		box.style.top = top;
+//	}
+//
+//	if(e.key == 'd'){ //move right
+//		left += 20;
+//		left += "px"
+//		box.style.left = left;
+//
+//		posy = 320;
+//		posx += 64;
+//		posx += "px";
+//		posy += "px";
+//		box.style.backgroundPositionX = posx;
+//		box.style.backgroundPositionY = posy;
+//
+//	}
+//
+//	if(e.key == 's'){ //move down
+//		top += 20;
+//		top += "px"
+//		box.style.top = top;
+//
+//		posy = 384;
+//		posx += 64;
+//		posx += "px";
+//		posy += "px";
+//		box.style.backgroundPositionX = posx;
+//		box.style.backgroundPositionY = posy;
+//
+//
+//	}
+//
+//	if(e.key == 'a'){ //move left
+//	left -= 20;
+//	left += "px"
+//	box.style.left = left;
+//
+//	posx -= 64;
+//	posx += "px";
+//	posy = 192;
+//	posy += "px";
+//	box.style.backgroundPositionX = posx;		box.style.backgroundPositionY = posy;
+//
+//	}
+//
+//	if(e.key == 'w'){ //move up
+//		top -= 20;
+//		top += "px"
+//		box.style.top = top;
+//
+//		posy = 256;
+//		posx += 64;
+//		posx += "px";
+//		posy += "px";
+//		box.style.backgroundPositionX = posx;
+//		box.style.backgroundPositionY = posy;
+//	}
+//    console.log(e.key);
+//    }
+//
+//document.onclick = function (coords){
+//	var fireball = document.getElementsByClassName("fireball")[0];
+//
+//
+//
+//
+//	var box = document.getElementsByTagName("box")[0];
+//	var top = window.getComputedStyle(box.nativeElement).top;
+//	top = Number(top.replace("px", ""));
+//	var left = window.getComputedStyle(box.nativeElement).left;
+//	left = Number(left.replace("px", ""));
+//
+//
+//
+//
+//	var xcoord = coords.x - 16;
+//	var ycoord = coords.y - 16;
+//
+//		xcoord += "px";
+//		ycoord += "px";
+//		fireball.style.left = xcoord;
+//		fireball.style.top = ycoord;
+//
+//if(((coords.x < left + 51) && (coords.x > left + 13)) && ((coords.y < top + 66) && (coords.y > top + 13))){
+//	console.log("hit");
+//	box.style.backgroundColor = "orange";
+//}
+//	else{
+//		box.style.backgroundColor = null;
+//	}
+//}
+//
+//function getFormValues(){
+//	var f = document.getElementsByName("f")[0];
+//	var l = document.getElementsByName("l")[0];
+//
+//	console.log(f.value);
+//    console.log(l.value);
+//}
+//
+//$("p").mouseover(e => {
+//	$("body").css("background-color", "yellow");
+//})
+//
+//$("p").mouseout(e => {
+//	$("body").css("background-color", "inherit");
+//})
