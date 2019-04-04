057553015652db2a582a9d983d575a81cff62e5d
Benjamin <40469901+BenjaminC137@users.noreply.github.com>
2019-04-03 20:22:34 -0500

    Put up photos

--- src/app/photographs.ts
+++ src/app/photographs.ts
@@ -4,15 +4,177 @@ export const Photographs : PhotographyModel[] = [
 	{
 		id: 1,
 		title: "Canadian Murals",
-		description: "Murals in Canada",
-		date: '2018/05/06',
-		imagePath: "assets/Photography/Canadian_murals-50percent-IMG_9447.png"
+		description: "Beautiful murals in a decrepit alley Canada.",
+		date: '2099/05/06',
+		imagePath: "assets/Photography/CanadianMurals.jpg"
 	},
 	{
 		id: 2,
-		title: "Water Tower",
-		description: "Water Tower",
-		date: '2011/05/19',
-		imagePath: "assets/Photography/water_tower-50percent-IMG_8202.png"
+		title: "Tower Of Fire",
+		description: "Chicago's Old Water Tower that is one of the few structures that survived the Great Chicago Fire of 1871.",
+		date: '2999',
+		imagePath: "assets/Photography/TowerOfFire.jpg"
+	},
+	{
+		id: 3,
+		title: "Fragile Innocence",
+		description: "Every moment of the youth carries forward eternally.",
+		date: '2999/05/06',
+		imagePath: "assets/Photography/FragileInnocence.jpg"
+	},
+	{
+		id: 4,
+		title: "Prison Of Freedom",
+		description: "Seagulls perched freely before the distant prison of Alcatraz.",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/PrisonOfFreedom.jpg"
+	},
+	{
+		id: 5,
+		title: "Are You There?",
+		description: "Are You There, God? Universe? Higher Power? Are you watching me? Are you caring for me?",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/AreYouThere.jpg"
+	},
+	{
+		id: 6,
+		title: "Chaos In Solitude",
+		description: "Distopian subways of Chicago. A man sits.",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/ChaosInSolitude.jpg"
+	},	{
+		id: 7,
+		title: "Downward Ascension",
+		description: "Upward. Downward. Timeless. Humanity remains into the past and into the future.",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/DownwardAscension.jpg"
+	},	{
+		id: 8,
+		title: "Enter Warm",
+		description: "The sun. It won't leave us forever.",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/EnterWarm.jpg"
+	},	{
+		id: 9,
+		title: "Entropic Dissonance",
+		description: "Shattered; a modern technology. Beauty.",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/EntropicDissonance.jpg"
+	},	{
+		id: 10,
+		title: "Entropy Vs Beauty",
+		description: "Entropy does not discount beauty. It may enrich it.",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/EntropyVsBeauty.jpg"
+	},	{
+		id: 11,
+		title: "Far Reach",
+		description: "I know this is there. What is it? Who are you?",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/FarReach.jpg"
+	},	{
+		id: 12,
+		title: "Forbidden Progression",
+		description: "New York. The railway to chaos. We cannot proceed.",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/ForbiddenProgression.jpg"
+	},	{
+		id: 13,
+		title: "Frozen Son",
+		description: "Cold. Warm. Son. Sun.",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/FrozenSon.jpg"
+	},	{
+		id: 14,
+		title: "Full Silence",
+		description: "Perfectly still scene in Colorado. Beauty that cannot be captured. It must be experienced. Nature has not fully been taken by the virus of humanity. Not yet.",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/FullSilence.jpg"
+	},	{
+		id: 15,
+		title: "Grounded Confluence",
+		description: "GroundedConfluence",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/GroundedConfluence.jpg"
+	},	{
+		id: 16,
+		title: "Infinite Void",
+		description: "InfiniteVoid",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/InfiniteVoid.jpg"
+	},	{
+		id: 17,
+		title: "Lonely Evolution",
+		description: "LonelyEvolution",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/LonelyEvolution.jpg"
+	},	{
+		id: 18,
+		title: "Merging Consciousness",
+		description: "MergingConsciousness",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/MergingConsciousness.jpg"
+	},	{
+		id: 19,
+		title: "Ominous Sky",
+		description: "OminousSky",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/OminousSky.jpg"
+	},	{
+		id: 20,
+		title: "Pause",
+		description: "Pause",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/Pause.jpg"
+	},
+		{
+		id: 21,
+		title: "Quadrillionth Kingdom",
+		description: "QuadrillionthKingdom",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/QuadrillionthKingdom.jpg"
+	},	{
+		id: 22,
+		title: "Safe Inside",
+		description: "SafeInside",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/SafeInside.jpg"
+	},	{
+		id: 23,
+		title: "She Knows",
+		description: "SheKnows",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/SheKnows.jpg"
+	},	{
+		id: 24,
+		title: "Sounds Of Solitude",
+		description: "SoundsOfSolitude",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/SoundsOfSolitude.jpg"
+	},	{
+		id: 25,
+		title: "Techno Convergence",
+		description: "TechnoConvergence",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/TechnoConvergence.jpg"
+	},	{
+		id: 26,
+		title: "The Breach Of The Singularity",
+		description: "TheBreachOfTheSingularity",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/TheBreachOfTheSingularity.jpg"
+	},	{
+		id: 27,
+		title: "Urban Perserverance",
+		description: "UrbanPerserverance",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/UrbanPerserverance.jpg"
+	},	{
+		id: 28,
+		title: "When Darkness Chooses",
+		description: "WhenDarknessChooses",
+		date: '2099/01/01',
+		imagePath: "assets/Photography/WhenDarknessChooses.jpg"
 	}
+
 ];
