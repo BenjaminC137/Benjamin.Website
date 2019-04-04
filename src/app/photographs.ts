a8e0741547da6f4dbe68fa5c044d70549e879f53
Benjamin <40469901+BenjaminC137@users.noreply.github.com>
2019-04-03 22:05:55 -0500

    Filled Photography with photographs & PR

--- src/app/photographs.ts
+++ src/app/photographs.ts
@@ -5,175 +5,175 @@ export const Photographs : PhotographyModel[] = [
 		id: 1,
 		title: "Canadian Murals",
 		description: "Beautiful murals in a decrepit alley Canada.",
-		date: '2099/05/06',
+		date: '2018/05/06',
 		imagePath: "assets/Photography/CanadianMurals.jpg"
 	},
 	{
 		id: 2,
 		title: "Tower Of Fire",
 		description: "Chicago's Old Water Tower that is one of the few structures that survived the Great Chicago Fire of 1871.",
-		date: '2999',
+		date: '2011/05/19',
 		imagePath: "assets/Photography/TowerOfFire.jpg"
 	},
 	{
 		id: 3,
 		title: "Fragile Innocence",
 		description: "Every moment of the youth carries forward eternally.",
-		date: '2999/05/06',
+		date: '2016/06/05',
 		imagePath: "assets/Photography/FragileInnocence.jpg"
 	},
 	{
 		id: 4,
 		title: "Prison Of Freedom",
 		description: "Seagulls perched freely before the distant prison of Alcatraz.",
-		date: '2099/01/01',
+		date: '2015/02/25',
 		imagePath: "assets/Photography/PrisonOfFreedom.jpg"
 	},
 	{
 		id: 5,
 		title: "Are You There?",
 		description: "Are You There, God? Universe? Higher Power? Are you watching me? Are you caring for me?",
-		date: '2099/01/01',
+		date: '2015/02/06',
 		imagePath: "assets/Photography/AreYouThere.jpg"
 	},
 	{
 		id: 6,
 		title: "Chaos In Solitude",
 		description: "Distopian subways of Chicago. A man sits.",
-		date: '2099/01/01',
+		date: '2011/10/07',
 		imagePath: "assets/Photography/ChaosInSolitude.jpg"
 	},	{
 		id: 7,
 		title: "Downward Ascension",
 		description: "Upward. Downward. Timeless. Humanity remains into the past and into the future.",
-		date: '2099/01/01',
+		date: '2018/03/17',
 		imagePath: "assets/Photography/DownwardAscension.jpg"
 	},	{
 		id: 8,
 		title: "Enter Warm",
 		description: "The sun. It won't leave us forever.",
-		date: '2099/01/01',
+		date: '2012/03/13',
 		imagePath: "assets/Photography/EnterWarm.jpg"
 	},	{
 		id: 9,
 		title: "Entropic Dissonance",
 		description: "Shattered; a modern technology. Beauty.",
-		date: '2099/01/01',
+		date: '2016/12/06',
 		imagePath: "assets/Photography/EntropicDissonance.jpg"
 	},	{
 		id: 10,
 		title: "Entropy Vs Beauty",
 		description: "Entropy does not discount beauty. It may enrich it.",
-		date: '2099/01/01',
+		date: '2012/11/02',
 		imagePath: "assets/Photography/EntropyVsBeauty.jpg"
 	},	{
 		id: 11,
 		title: "Far Reach",
 		description: "I know this is there. What is it? Who are you?",
-		date: '2099/01/01',
+		date: '2017/11/19',
 		imagePath: "assets/Photography/FarReach.jpg"
 	},	{
 		id: 12,
 		title: "Forbidden Progression",
 		description: "New York. The railway to chaos. We cannot proceed.",
-		date: '2099/01/01',
+		date: '2012/10/31',
 		imagePath: "assets/Photography/ForbiddenProgression.jpg"
 	},	{
 		id: 13,
 		title: "Frozen Son",
 		description: "Cold. Warm. Son. Sun.",
-		date: '2099/01/01',
+		date: '2018/03/17',
 		imagePath: "assets/Photography/FrozenSon.jpg"
 	},	{
 		id: 14,
 		title: "Full Silence",
 		description: "Perfectly still scene in Colorado. Beauty that cannot be captured. It must be experienced. Nature has not fully been taken by the virus of humanity. Not yet.",
-		date: '2099/01/01',
+		date: '2018/02/09',
 		imagePath: "assets/Photography/FullSilence.jpg"
 	},	{
 		id: 15,
 		title: "Grounded Confluence",
-		description: "GroundedConfluence",
-		date: '2099/01/01',
+		description: "One with nature. Suffers and survives. Cycles through.",
+		date: '2017/02/11',
 		imagePath: "assets/Photography/GroundedConfluence.jpg"
 	},	{
 		id: 16,
 		title: "Infinite Void",
-		description: "InfiniteVoid",
-		date: '2099/01/01',
+		description: "This repeats indefinitely. We'll never get there. We'll always not be reached by the end.",
+		date: '2014/02/05',
 		imagePath: "assets/Photography/InfiniteVoid.jpg"
 	},	{
 		id: 17,
 		title: "Lonely Evolution",
-		description: "LonelyEvolution",
-		date: '2099/01/01',
+		description: "The destructin of our planet. Innocent. Naive. Beautiful. Inevitable. Acceptable.",
+		date: '2012/11/02',
 		imagePath: "assets/Photography/LonelyEvolution.jpg"
 	},	{
 		id: 18,
 		title: "Merging Consciousness",
-		description: "MergingConsciousness",
-		date: '2099/01/01',
+		description: "The bridge between Human Consciousness and Artificial intelligence. I don't know if it is good or bad. It is, however, going to be beautiful.",
+		date: '2014/01/22',
 		imagePath: "assets/Photography/MergingConsciousness.jpg"
 	},	{
 		id: 19,
 		title: "Ominous Sky",
-		description: "OminousSky",
-		date: '2099/01/01',
+		description: "We have been chosen.",
+		date: '2012/07/01',
 		imagePath: "assets/Photography/OminousSky.jpg"
 	},	{
 		id: 20,
 		title: "Pause",
-		description: "Pause",
-		date: '2099/01/01',
+		description: "We don't have time, but time has us. It's time to embrace that which embraces us.",
+		date: '2015/02/25',
 		imagePath: "assets/Photography/Pause.jpg"
 	},
 		{
 		id: 21,
 		title: "Quadrillionth Kingdom",
-		description: "QuadrillionthKingdom",
-		date: '2099/01/01',
+		description: "We're among the stars. We've been gifted with an escape from the horror of the unknown. The concept of time. The vastness of the universe. Right here. Here, we have beauty. That which remains. Some fight for preservation. The stars are still.",
+		date: '2015/02/26',
 		imagePath: "assets/Photography/QuadrillionthKingdom.jpg"
 	},	{
 		id: 22,
 		title: "Safe Inside",
-		description: "SafeInside",
-		date: '2099/01/01',
+		description: "I hear the rain. It is beautiful. From inside, I can hide.",
+		date: '2018/01/11',
 		imagePath: "assets/Photography/SafeInside.jpg"
 	},	{
 		id: 23,
 		title: "She Knows",
-		description: "SheKnows",
-		date: '2099/01/01',
+		description: "You cannot hide. You needn't.",
+		date: '2010/12/17',
 		imagePath: "assets/Photography/SheKnows.jpg"
 	},	{
 		id: 24,
 		title: "Sounds Of Solitude",
-		description: "SoundsOfSolitude",
-		date: '2099/01/01',
+		description: "We have an option. In here, it is quiet.",
+		date: '2012/10/16',
 		imagePath: "assets/Photography/SoundsOfSolitude.jpg"
 	},	{
 		id: 25,
 		title: "Techno Convergence",
-		description: "TechnoConvergence",
-		date: '2099/01/01',
+		description: "Modern technology is magical and amazing. It is very simple when broken down. Everything is.",
+		date: '2015/02/10',
 		imagePath: "assets/Photography/TechnoConvergence.jpg"
 	},	{
 		id: 26,
 		title: "The Breach Of The Singularity",
-		description: "TheBreachOfTheSingularity",
-		date: '2099/01/01',
+		description: "What are we about to encounter? It is near. It is innevitable. The technology singularity is nigh.",
+		date: '2010/12/04',
 		imagePath: "assets/Photography/TheBreachOfTheSingularity.jpg"
 	},	{
 		id: 27,
 		title: "Urban Perserverance",
-		description: "UrbanPerserverance",
-		date: '2099/01/01',
+		description: "Creation is the beginning of entropy.",
+		date: '2018/10/14',
 		imagePath: "assets/Photography/UrbanPerserverance.jpg"
 	},	{
 		id: 28,
 		title: "When Darkness Chooses",
-		description: "WhenDarknessChooses",
-		date: '2099/01/01',
+		description: "Have we been chosen?",
+		date: '2012/09/05',
 		imagePath: "assets/Photography/WhenDarknessChooses.jpg"
 	}
 
